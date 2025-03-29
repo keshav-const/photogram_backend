6e6997ab0fe2338c4c81e37a07e0ead866fc797a
keshav-const <keshavladdha005@gmail.com>
2025-03-29 14:09:29 +0530

    fix1

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ app.use(cookieParser());
 app.use(helmet());
 app.use(cors({
     origin: ["http://localhost:3000",
-        "https://photogram-frontend-ctxg.vercel.app"
+        "https://photogram-frontend-a9kg.vercel.app"
 
     ],
     credentials: true,
